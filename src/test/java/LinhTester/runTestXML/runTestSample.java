3c9735c0ac65a7e5a497504b5db6789997712503
LeLinh2712 <lelinh27121@gmail.com>
2023-12-23 21:54:29 +0700

    code testNG

---
+++ src/test/java/LinhTester/runTestXML/runTestSample.java
@@ -0,0 +1,15 @@
+package LinhTester.runTestXML;
+
+import org.testng.annotations.Test;
+
+public class runTestSample {
+    @Test
+    public void firstTest() {
+        System.out.println("First test method");
+    }
+
+    @Test
+    public void secondTest() {
+        System.out.println("Second test method");
+    }
+}
